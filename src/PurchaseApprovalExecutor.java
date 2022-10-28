7e0d0aec8e2fe960379c947b438709b69f34baea
Valeri Yanchev <valerifalko@gmail.com>
2022-10-28 11:31:12 +0300

    initial commit

--- src/PurchaseApprovalExecutor.java
+++ src/PurchaseApprovalExecutor.java
@@ -2,6 +2,8 @@ import common.Type;
 import handlers.Approver;
 import handlers.Manager;
 
+import java.util.Scanner;
+
 /**
  * Execution class of the application.
  * Be free to modify below line 14 (bellow comment line)
@@ -13,9 +15,22 @@ public class PurchaseApprovalExecutor {
         ApprovalChainGenerator.generate(manager);
         //Be free to modify method below this line
 
-        manager.approve(1, 8001, Type.PC);
-        manager.approve(2, 4900, Type.PC);
-        manager.approve(3, 5000, Type.GAMING);
-        manager.approve(4, 3000, Type.CLERICAL);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your ID:");
+        int id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter the price for new device:");
+        double cost = Double.parseDouble(scanner.nextLine());
+        System.out.println("Enter the type of the new device ( only with capital letters ):");
+        String type = scanner.nextLine();
+
+        manager.approve(id, cost, Type.valueOf(type));
+
+
+
+
+        //manager.approve(1, 8001, Type.PC);
+        //manager.approve(2, 4900, Type.PC);
+        //manager.approve(3, 5000, Type.GAMING);
+        //manager.approve(4, 3000, Type.CLERICAL);
     }
 }
